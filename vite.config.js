b0902171257ba8e14d5805b850206ef2e2a4a4b6
sangw214 <hybridspirit@gmail.com>
2023-11-05 11:23:22 +0900

    basepath update,,

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/VITE-CAMERA-APP/',
+  base:'/vite-camera-app/',
   plugins: [react()],
 })
